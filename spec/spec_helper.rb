62b840ce3df49bbc1c365c994bb1211d6695dec3
Fabio Rehm <fgrehm@gmail.com>
2013-01-03 23:25:53 -0200

    Tweaks to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,14 @@ require 'capybara/rails'
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
 end
