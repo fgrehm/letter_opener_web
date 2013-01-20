e255b4415c6a0528895a9e2e51617c8ce90b1d56
Fabio Rehm <fgrehm@gmail.com>
2013-01-20 19:13:08 -0200

    Add button to reload emails using AJAX. Closes #4

--- app/assets/javascripts/letter_opener_web/application.js
+++ app/assets/javascripts/letter_opener_web/application.js
@@ -1,23 +1,19 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery-1.8.3.min
 //= require_tree .
 
 jQuery(function($) {
-  $('.letter-opener tr').click(function() {
+  $('.letter-opener').on('click', 'tr', function() {
     var $this = $(this);
     $('iframe').attr('src', $this.find('a').attr('href'));
     $this.parent().find('.active').removeClass('active');
     $this.addClass('active');
+  });
+
+  $('.refresh').click(function(e) {
+    e.preventDefault();
+
+    var table = $('.letter-opener');
+    table.find('tbody').empty().append('<tr><td colspan="2">Loading...</td></tr>');
+    table.load(table.data('letters-path') + ' .letter-opener');
   });
 });
