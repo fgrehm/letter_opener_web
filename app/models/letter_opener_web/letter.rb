7f4adb5e8cda42ea9fa6280f4652d52362fd9694
Fabio Rehm <fgrehm@gmail.com>
2013-01-20 18:55:02 -0200

    Add comment to explain why we cant feed the whole email file contents to an XML parser

--- app/models/letter_opener_web/letter.rb
+++ app/models/letter_opener_web/letter.rb
@@ -43,6 +43,10 @@ module LetterOpenerWeb
     def read_file(style)
       contents = File.read("#{letters_location}/#{id}/#{style}.html")
 
+      # We cannot feed the whole file to an XML parser as some mails are
+      # "complete" (as in they have the whole <html> structure) and letter_opener
+      # prepends some information about the mail being sent, making REXML
+      # complain about it
       contents.scan(/<a[^>]+>.+<\/a>/).each do |link|
         xml = REXML::Document.new(link).root
         unless xml.attributes['href'] =~ /(plain|rich).html/
