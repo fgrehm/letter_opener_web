015c5f2230ba811cb2b1691f17441d3923ede601
josisusan <josisusan@gmail.com>
2014-03-15 13:25:03 +0545

    Add routes for delete method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LetterOpenerWeb::Engine.routes.draw do
-  delete 'clear'                 => 'letters#clear', :as => :clear_letters
-  get    '/'                     => 'letters#index', :as => :letters
-  get    ':id(/:style)'          => 'letters#show',  :as => :letter
+  delete 'clear'                 => 'letters#clear',    :as => :clear_letters
+  delete ':id/delete'            => 'letters#destroy',  :as => :delete_letter
+  get    '/'                     => 'letters#index',    :as => :letters
+  get    ':id(/:style)'          => 'letters#show',     :as => :letter
   get    ':id/attachments/:file' => 'letters#attachment'
 end
