542a14a98fbe475379d5e8cb4e7c54de188ff9d8
Fabio Rehm <fgrehm@gmail.com>
2015-02-02 20:08:09 -0700

    Remove unecessary delete suffix from the action that deletes a single email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LetterOpenerWeb::Engine.routes.draw do
   delete 'clear'                 => 'letters#clear',    :as => :clear_letters
-  delete ':id/delete'            => 'letters#destroy',  :as => :delete_letter
+  delete ':id'                   => 'letters#destroy',  :as => :delete_letter
   get    '/'                     => 'letters#index',    :as => :letters
   get    ':id(/:style)'          => 'letters#show',     :as => :letter
   get    ':id/attachments/:file' => 'letters#attachment'
