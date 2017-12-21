9c4ebe0f99106665c8e2fbbedf20ff95c0b0001c
David Muto (pseudomuto) <david.muto@gmail.com>
2017-12-21 10:09:59 -0500

    Disable IndentHeredoc so ruby <2.3 works right

--- spec/models/letter_opener_web/letter_spec.rb
+++ spec/models/letter_opener_web/letter_spec.rb
@@ -4,16 +4,16 @@ describe LetterOpenerWeb::Letter do
   let(:location) { File.expand_path('../../../tmp', __FILE__) }
 
   def rich_text(mail_id)
-    <<~MAIL
-      Rich text for #{mail_id}
-      <!DOCTYPE html>
-      <a href='a-link.html'>
-        <img src='an-image.jpg'>
-        Link text
-      </a>
-      <a href='fooo.html'>Bar</a>
-      <a href="example.html" class="blank"></a>
-      <address><a href="inside-address.html">inside address</a></address>
+    <<-MAIL
+Rich text for #{mail_id}
+<!DOCTYPE html>
+<a href='a-link.html'>
+  <img src='an-image.jpg'>
+  Link text
+</a>
+<a href='fooo.html'>Bar</a>
+<a href="example.html" class="blank"></a>
+<address><a href="inside-address.html">inside address</a></address>
 MAIL
   end
 
