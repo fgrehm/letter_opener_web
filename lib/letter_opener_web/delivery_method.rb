2c7b1f45a90ca34c255b320cd2880ca8a2500d55
Vasiliy Ermolovich <younash@gmail.com>
2018-01-02 20:10:53 +0300

    Disable Launchy using ENV variable instead of completely redefining `deliver!` method.

--- lib/letter_opener_web/delivery_method.rb
+++ lib/letter_opener_web/delivery_method.rb
@@ -5,8 +5,9 @@ require 'letter_opener/delivery_method'
 module LetterOpenerWeb
   class DeliveryMethod < LetterOpener::DeliveryMethod
     def deliver!(mail)
-      location = File.join(settings[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
-      LetterOpener::Message.rendered_messages(location, mail)
+      ENV['LAUNCHY_DRY_RUN'] = 'true'
+      super
+      ENV['LAUNCHY_DRY_RUN'] = 'false'
     end
   end
 end
