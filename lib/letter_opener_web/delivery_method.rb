c67544d05ee64fd68650e14829562afbc804b504
Keith Pitt <me@keithpitt.com>
2018-01-10 11:22:24 +0800

    Updated to match the #deliver! in `letter_opener`

--- lib/letter_opener_web/delivery_method.rb
+++ lib/letter_opener_web/delivery_method.rb
@@ -5,8 +5,10 @@ require 'letter_opener/delivery_method'
 module LetterOpenerWeb
   class DeliveryMethod < LetterOpener::DeliveryMethod
     def deliver!(mail)
-      location = File.join(settings[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
-      LetterOpener::Message.rendered_messages(mail, location: location)
+      check_delivery_params(mail) if respond_to?(:check_delivery_params)
+      location = File.join(settings[:location], "#{Time.now.to_f.to_s.tr('.', '_')}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
+
+      LetterOpener::Message.rendered_messages(mail, location: location, message_template: settings[:message_template])
     end
   end
 end
