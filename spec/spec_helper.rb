31dc0f0e5fd58a7c919663b89004ca4973e95809
David Muto (pseudomuto) <david.muto@gmail.com>
2020-01-29 20:46:23 -0500

    Setup codecov report in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+if ENV.fetch('CI', '') == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'shoulda-matchers'
 
 RSpec.configure do |config|
