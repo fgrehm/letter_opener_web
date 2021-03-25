734ef652b1e41ccb85c1d2451dbef240617ab330
Shigenobu Nishikawa <shishi@srevo.net>
2021-03-25 15:44:22 +0900

    explicit letter class

--- app/controllers/letter_opener_web/letters_controller.rb
+++ app/controllers/letter_opener_web/letters_controller.rb
@@ -10,7 +10,7 @@ module LetterOpenerWeb
     before_action :load_letter, only: %i[show attachment destroy]
 
     def index
-      @letters = Letter.search
+      @letters = LetterOpenerWeb::Letter.search
     end
 
     def show
@@ -31,7 +31,7 @@ module LetterOpenerWeb
     end
 
     def clear
-      Letter.destroy_all
+      LetterOpenerWeb::Letter.destroy_all
       redirect_to routes.letters_path
     end
 
@@ -47,7 +47,7 @@ module LetterOpenerWeb
     end
 
     def load_letter
-      @letter = Letter.find(params[:id])
+      @letter = LetterOpenerWeb::Letter.find(params[:id])
       head :not_found unless @letter.exists?
     end
 
