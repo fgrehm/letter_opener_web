775829684513f3d0249cb1d0115f274774fecf1e
Paweł Dąbrowski <dziamber@gmail.com>
2024-01-25 19:50:07 +0100

    Add support for storing letters and attachments on AWS s3

---
+++ app/models/letter_opener_web/aws_letter.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+module LetterOpenerWeb
+  class AwsLetter < LetterOpenerWeb::Letter
+    def self.search
+      letters = LetterOpenerWeb.aws_client.list_objects_v2(bucket: LetterOpenerWeb.config.aws_bucket, delimiter: '/').common_prefixes.map do |folder|
+        new(id: folder.prefix.gsub('/', ''))
+      end
+    end
+
+    def self.destroy_all
+      letters = LetterOpenerWeb.aws_client.list_objects_v2(bucket: LetterOpenerWeb.config.aws_bucket).contents.map(&:key)
+
+      LetterOpenerWeb.aws_client.delete_objects(
+        bucket: LetterOpenerWeb.config.aws_bucket,
+        delete: {
+          objects: letters.map { |key| { key: key } },
+          quiet: false
+        }
+      )
+    end
+
+    def initialize(params)
+      @id = params.fetch(:id)
+    end
+
+    def attachments
+      @attachments ||= LetterOpenerWeb.aws_client.list_objects_v2(
+        bucket: LetterOpenerWeb.config.aws_bucket, prefix: "#{@id}/attachments/"
+      ).contents.each_with_object({}) do |file, hash|
+        hash[File.basename(file.key)] = attachment_url(file.key)
+      end
+    end
+
+    def delete
+      return unless valid?
+
+      letters = LetterOpenerWeb.aws_client.list_objects_v2(bucket: LetterOpenerWeb.config.aws_bucket, prefix: @id).contents.map(&:key)
+
+      LetterOpenerWeb.aws_client.delete_objects(
+        bucket: LetterOpenerWeb.config.aws_bucket,
+        delete: {
+          objects: letters.map { |key| { key: key } },
+          quiet: false
+        }
+      )
+    end
+
+    def valid?
+      LetterOpenerWeb.aws_client.list_objects_v2(bucket: LetterOpenerWeb.config.aws_bucket, prefix: @id).contents.any?
+    end
+
+    private
+
+    def attachment_url(key)
+      bucket = Aws::S3::Bucket.new(
+        name: LetterOpenerWeb.config.aws_bucket, client: LetterOpenerWeb.aws_client
+      )
+
+      obj = bucket.object(key)
+      obj.presigned_url(:get, expires_in: 1.week.to_i)
+    end
+
+    def objects
+      @objects ||= {}
+    end
+
+    def read_file(style)
+      return objects[style] if objects.key?(style)
+
+      response = LetterOpenerWeb.aws_client.get_object(bucket: LetterOpenerWeb.config.aws_bucket, key: "#{@id}/#{style}.html")
+
+      response.body.read.tap do |value|
+        objects[style] = value
+      end
+    rescue Aws::S3::Errors::NoSuchKey
+      ''
+    end
+
+    def style_exists?(style)
+      return !objects[style].empty? if objects.key?(style)
+
+      objects[style] = read_file(style)
+      !objects[style].empty?
+    end
+  end
+end
--- lib/letter_opener_web.rb
+++ lib/letter_opener_web.rb
@@ -3,10 +3,11 @@
 require 'letter_opener_web/version'
 require 'letter_opener_web/engine'
 require 'rexml/document'
+require 'aws-sdk-s3'
 
 module LetterOpenerWeb
   class Config
-    attr_accessor :letters_location
+    attr_accessor :letters_location, :aws_access_key_id, :aws_secret_access_key, :aws_region, :aws_bucket
   end
 
   def self.config
@@ -21,5 +22,14 @@ module LetterOpenerWeb
 
   def self.reset!
     @config = nil
+    @aws_client = nil
+  end
+
+  def self.aws_client
+    @aws_client ||= ::Aws::S3::Client.new(
+      access_key_id: LetterOpenerWeb.config.aws_access_key_id,
+      secret_access_key: LetterOpenerWeb.config.aws_secret_access_key,
+      region: LetterOpenerWeb.config.aws_region
+    )
   end
 end
