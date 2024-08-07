7491cbcc55a9cd2fb244e96abe986a2d1d0b1e52
Étienne Barrié <etienne.barrie@gmail.com>
2024-08-07 11:11:15 +0200

    Remove deprecated calls to routing mapper
    See https://github.com/rails/rails/pull/52422

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 LetterOpenerWeb::Engine.routes.draw do
-  get  '/'                     => 'letters#index',    as: :letters
-  post 'clear'                 => 'letters#clear',    as: :clear_letters
-  get  ':id(/:style)'          => 'letters#show',     as: :letter
-  post ':id/delete'            => 'letters#destroy',  as: :delete_letter
-  get  ':id/attachments/:file' => 'letters#attachment', constraints: { file: %r{[^/]+} }
+  get  '/',                     to: 'letters#index',    as: :letters
+  post 'clear',                 to: 'letters#clear',    as: :clear_letters
+  get  ':id(/:style)',          to: 'letters#show',     as: :letter
+  post ':id/delete',            to: 'letters#destroy',  as: :delete_letter
+  get  ':id/attachments/:file', to: 'letters#attachment', constraints: { file: %r{[^/]+} }
 end
